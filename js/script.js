abb814b9006f4066b36490aaf7ac74fc8d30f13c
David McGarvey <davidjmcgarvey@gmail.com>
2019-09-26 10:19:00 -0700

    in progress: one modal renders, then breaks

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 // ------------------------------------------
 //  Create Card
 // ------------------------------------------
-// const currentEmployees = [];
+const employeeList = [];
 function createCard(response) {
+  
   for (let i = 0; i < 12; i++) {
     const card = document.createElement('div');
     const firstName = response.results[i].name.first;
     const lastName = response.results[i].name.last;
     const fullName = `${firstName} ${lastName}`;
     const email = response.results[i].email;
+    const homeStreet = response.results[i].location.street;
     const homeCity = response.results[i].location.city;
     const homeState = response.results[i].location.state;
     const profilePic = response.results[i].picture.large;
+    const phone = response.results[i].phone;
+    const birthday = response.results[i].dob.date;
+    const employeeAttr = { // attributes to be used on the Modal later: pic, name, email, city, phone, address, birthday
+      'pic': profilePic,
+      'name': fullName,
+      'email': email,
+      'street': homeStreet,
+      'city': homeCity,
+      'state': homeState,
+      'phone': phone,
+      'birthday': birthday
+    };
     card.className = 'card';
     card.innerHTML = `
                       <div class="card-img-container">
@@ -24,11 +38,11 @@ function createCard(response) {
                       </div>
                       `;
     document.querySelector('#gallery').append(card);
+    // console.log(employeeAttr);
+    // $('.modal-container').hide();
+    employeeList.push(employeeAttr);
   }
-  // for (let i = 0; i < 12; i++) {
-  //   const employee = response.results[i];
-  //   currentEmployees.push(employee);
-  // }
+  console.log(employeeList);
 }
 
 // ------------------------------------------
@@ -60,6 +74,30 @@ function createModal() {
   card.append(modal);
 }
 
+function updateModal(modal, index) {
+  const currentModal = document.querySelectorAll('.modal-container');
+  // console.log(currentModal[index].innerHTML);
+  currentModal[index].innerHTML = `
+                          <div class="modal">
+                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                            <div class="modal-info-container">
+                              <img class="modal-img" src="${modal[index].pic}" alt="profile picture">
+                              <h3 id="name" class="modal-name cap">${modal[index].name}</h3>
+                              <p class="modal-text">${modal[index].email}</p>
+                              <p class="modal-text cap">${modal[index].city}</p>
+                              <hr>
+                              <p class="modal-text">${modal[index].phone}</p>
+                              <p class="modal-text">${modal[index].street}, ${modal[index].city}, OR 97204</p>
+                              <p class="modal-text">Birthday: ${modal[index].birthday}</p>
+                            </div>
+                          </div>
+                          <div class="modal-btn-container">
+                            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                          </div>
+                          `;
+}
+
 // ------------------------------------------
 //  Create Search
 // ------------------------------------------
@@ -81,8 +119,8 @@ function fetchData(url) {
   fetch(url)
     .then(checkStatus)
     .then(response => response.json())
-    .then(createSearch())
     .then(response => createCard(response))
+    .then(createSearch())
     .catch(error => console.log('There was a problem dawg!', error))
     .finally(createModal());
 }
@@ -102,10 +140,20 @@ fetchData('https://randomuser.me/api/?results=12');
 // ------------------------------------------
 $('.modal-container').hide();
 
-$('#gallery').on('click', 'div.card', () => console.log('DAVE DAWG!!')); // Click handler WAS not working! Is it a async thing?
+// $('#gallery').on('click', 'div.card', () => console.log('DAVE DAWG!!')); // Click handler WAS not working! Is it a async thing?
 
-$('#gallery').on('click', 'div.card', () => {
-  $('.modal-container').show();
+$('#gallery').on('click', 'div.card', (event) => {
+  const thisCard = document.querySelectorAll('div.card');  
+  const cardTarget = event.currentTarget;
+  // console.log(cardTarget.innerHTML);
+  // $('.modal-container').show();
+  for (let i = 0; i < 12; i++) {
+    if (thisCard[i].innerHTML === cardTarget.innerHTML) {
+      updateModal(employeeList, i);
+      $('.modal-container').show();
+      console.log(thisCard[i]);
+    }
+  }
 })
 
 $('#modal-prev').on('click', () => {
